ef8d5e2f0b8794825dc22d51a5805c2acae9d96d
Chirag Ramachandraiah <chirag_ramachandraiah@external.mckinsey.com>
2019-01-26 20:07:24 +0530

    Code and test for hello hapi exercise

---
+++ hellohapi.js
@@ -0,0 +1,30 @@
+const Hapi = require('hapi');
+
+
+const server = Hapi.Server({
+    host: 'localhost',
+    port: Number(process.argv[2]) || 8080
+    });
+
+    server.route({
+        path: '/',
+        method: 'GET',
+        handler: (request, h) => {
+            return 'Hello hapi';
+            }
+    });
+
+const init =async () =>{
+    await server.start();
+}
+
+console.log(`Server running at: ${server.info.uri}`);
+
+process.on('SIGINT',()=>{
+    server.stop({ timeout: 10000 }).then(function (err) {
+        console.log('hapi server stopped')
+        process.exit((err) ? 1 : 0)
+        });
+});    
+
+module.exports = server;
---
+++ hellohapi.test.js
@@ -0,0 +1,21 @@
+const server = require('./hellohapi.js');
+
+describe('Checking if',()=>{
+    
+    const options = {
+        url: '/',
+        method: 'GET'
+    }
+
+    it('Server returns a specific string',async ()=>{
+        const response = await server.inject(options);
+
+        expect(response.result).toEqual('Hello hapi');
+    });
+
+    it('Server returns a specific string',async ()=>{
+        const response = await server.inject(options);
+
+        expect(response.result).not.toEqual('');
+    });
+});
